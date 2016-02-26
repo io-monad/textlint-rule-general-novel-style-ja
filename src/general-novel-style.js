9c1e999d181d1691ea1391e261ab83d9dc92392a
io-monad <iride.monad@gmail.com>
2016-02-27 03:17:06 +0900

    Add line and column to reported errors

--- src/general-novel-style.js
+++ src/general-novel-style.js
@@ -48,6 +48,23 @@ export default function (context, options = {}) {
 
             return new Promise((resolve, reject) => {
                 const text = getSource(node);
+                const lines = text.split(/\r?\n/);
+
+                const reportMatches = (props) => {
+                    const {pattern, test, message} = props;
+                    let matches;
+                    lines.forEach((line, index) => {
+                        pattern.lastIndex = 0;
+                        while (matches = pattern.exec(line)) {
+                            if (!test || test(matches)) {
+                                report(node, new RuleError(message, {
+                                    line: index,
+                                    column: matches.index
+                                }));
+                            }
+                        }
+                    });
+                };
 
                 if (charsLeadingParagraph.length > 0) {
                     if (charsLeadingParagraph.indexOf(text.charAt(0)) == -1) {
@@ -56,54 +73,62 @@ export default function (context, options = {}) {
                 }
 
                 if (noPunctuationAtClosingQuote) {
-                    if (/[。、][」』】〉》）\)”"’'］\]〕｝\}＞>]/.test(text)) {
-                        report(node, new RuleError("句読点(。、)が閉じ括弧の直前に存在しています"));
-                    }
+                    reportMatches({
+                        pattern: /[。、][」』】〉》）\)”"’'］\]〕｝\}＞>]/g,
+                        message: "句読点(。、)が閉じ括弧の直前に存在しています"
+                    });
                 }
 
                 if (spaceAfterMarks) {
-                    if (/[？！](?![　？！」』】〉》）\)”"’'］\]〕｝\}＞>]|$)/.test(text)) {
-                        report(node, new RuleError("感嘆符(！)・疑問符(？)の直後にスペースか閉じ括弧が必要です"));
-                    }
+                    reportMatches({
+                        pattern: /[？！](?![　？！」』】〉》）\)”"’'］\]〕｝\}＞>]|$)/g,
+                        message: "感嘆符(！)・疑問符(？)の直後にスペースか閉じ括弧が必要です"
+                    });
                 }
 
                 if (evenNumberEllipsises) {
-                    let matches1 = text.match(/…+/g);
-                    if (matches1 && matches1.some((s) => { return s.length % 2 == 1 })) {
-                        report(node, new RuleError("連続した三点リーダー(…)の数が偶数ではありません"));
-                    }
+                    reportMatches({
+                        pattern: /…+/g,
+                        test:    (matches) => { return matches[0].length % 2 == 1 },
+                        message: "連続した三点リーダー(…)の数が偶数ではありません"
+                    });
                 }
 
                 if (evenNumberDashes) {
-                    let matches2 = text.match(/―+/g);
-                    if (matches2 && matches2.some((s) => { return s.length % 2 == 1 })) {
-                        report(node, new RuleError("連続したダッシュ(―)の数が偶数ではありません"));
-                    }
+                    reportMatches({
+                        pattern: /―+/g,
+                        test:    (matches) => { return matches[0].length % 2 == 1 },
+                        message: "連続したダッシュ(―)の数が偶数ではありません"
+                    });
                 }
 
                 if (appropriateUseOfInterpunct) {
-                    if (/・・+/.test(text)) {
-                        report(node, new RuleError("連続した中黒(・)が使われています"));
-                    }
+                    reportMatches({
+                        pattern: /・・+/g,
+                        message: "連続した中黒(・)が使われています"
+                    });
                 }
 
                 if (appropriateUseOfChoonpu) {
-                    if (/ーー+/.test(text)) {
-                        report(node, new RuleError("連続した長音符(ー)が使われています"))
-                    }
+                    reportMatches({
+                        pattern: /ーー+/g,
+                        message: "連続した長音符(ー)が使われています"
+                    });
                 }
 
                 if (appropriateUseOfMinusSign) {
-                    if (/−(?![0-9０１２３４５６７８９〇一二三四五六七八九十])/.test(text)) {
-                        report(node, new RuleError("マイナス記号(−)の直後が数字ではありません"));
-                    }
+                    reportMatches({
+                        pattern: /−(?![0-9０１２３４５６７８９〇一二三四五六七八九十])/g,
+                        message: "マイナス記号(−)の直後が数字ではありません"
+                    });
                 }
 
                 if (typeof maxArabicNumeralDigits == "number") {
-                    let matches3 = text.match(/[0-9０１２３４５６７８９]+/g);
-                    if (matches3 && matches3.some((s) => { return s.length > maxArabicNumeralDigits })) {
-                        report(node, new RuleError(`${maxArabicNumeralDigits}桁を超えるアラビア数字が使われています`));
-                    }
+                    reportMatches({
+                        pattern: /[0-9０１２３４５６７８９]+/g,
+                        test:    (matches) => { return matches[0].length > maxArabicNumeralDigits },
+                        message: `${maxArabicNumeralDigits}桁を超えるアラビア数字が使われています`
+                    });
                 }
 
                 resolve();
--- test/general-novel-style-test.js
+++ test/general-novel-style-test.js
@@ -57,44 +57,134 @@ tester.run("general-novel-style", rule, {
     invalid: [
         {
             text: "＃日本語の小説のテストです。",
-            errors: [{ message: "段落の先頭に許可されていない文字が存在しています" }]
+            errors: [
+                {
+                    message: "段落の先頭に許可されていない文字が存在しています",
+                    line: 1,
+                    column: 1
+                }
+            ]
         },
         {
             text: "「こんにちは、世界。」",
-            errors: [{ message: "句読点(。、)が閉じ括弧の直前に存在しています" }]
+            errors: [
+                {
+                    message: "句読点(。、)が閉じ括弧の直前に存在しています",
+                    line: 1,
+                    column: 10
+                }
+            ]
         },
         {
             text: "「これはペンですか？いや、ペンであるはずがない！」",
-            errors: [{ message: "感嘆符(！)・疑問符(？)の直後にスペースか閉じ括弧が必要です" }]
+            errors: [
+                {
+                    message: "感嘆符(！)・疑問符(？)の直後にスペースか閉じ括弧が必要です",
+                    line: 1,
+                    column: 10
+                }
+            ]
         },
         {
-            text: "「………なん…だと…」",
-            errors: [{ message: "連続した三点リーダー(…)の数が偶数ではありません" }]
+            text: "「………なん…だと……」",
+            errors: [
+                {
+                    message: "連続した三点リーダー(…)の数が偶数ではありません",
+                    line: 1,
+                    column: 2
+                },
+                {
+                    message: "連続した三点リーダー(…)の数が偶数ではありません",
+                    line: 1,
+                    column: 7
+                }
+            ]
         },
         {
             text: "『一体いつから―――ペンであると錯覚していた――？』",
-            errors: [{ message: "連続したダッシュ(―)の数が偶数ではありません" }]
+            errors: [
+                {
+                    message: "連続したダッシュ(―)の数が偶数ではありません",
+                    line: 1,
+                    column: 8
+                }
+            ]
         },
         {
             text: "　ヘロー・・・ワールド・・・",
-            errors: [{ message: "連続した中黒(・)が使われています" }]
+            errors: [
+                {
+                    message: "連続した中黒(・)が使われています",
+                    line: 1,
+                    column: 5
+                },
+                {
+                    message: "連続した中黒(・)が使われています",
+                    line: 1,
+                    column: 12
+                }
+            ]
         },
         {
             text: "　ヘローーーー・ワーーーールド",
-            errors: [{ message: "連続した長音符(ー)が使われています" }]
+            errors: [
+                {
+                    message: "連続した長音符(ー)が使われています",
+                    line: 1,
+                    column: 4
+                },
+                {
+                    message: "連続した長音符(ー)が使われています",
+                    line: 1,
+                    column: 10
+                },
+            ]
         },
         {
             text: "　ヘロ−・ワ−ルド",
-            errors: [{ message: "マイナス記号(−)の直後が数字ではありません" }]
+            errors: [
+                {
+                    message: "マイナス記号(−)の直後が数字ではありません",
+                    line: 1,
+                    column: 4
+                },
+                {
+                    message: "マイナス記号(−)の直後が数字ではありません",
+                    line: 1,
+                    column: 7
+                }
+            ]
         },
         {
-            text: "　１２３４＋３２１＝１５５５",
-            errors: [{ message: "2桁を超えるアラビア数字が使われています" }]
+            text: "　１２３４＋２１＝１２５５",
+            errors: [
+                {
+                    message: "2桁を超えるアラビア数字が使われています",
+                    line: 1,
+                    column: 2
+                },
+                {
+                    message: "2桁を超えるアラビア数字が使われています",
+                    line: 1,
+                    column: 10
+                }
+            ]
         },
         {
             text: "　１２３４＋３２１＝１５５５",
             options: { "max_arabic_numeral_digits": 3 },
-            errors: [{ message: "3桁を超えるアラビア数字が使われています" }]
+            errors: [
+                {
+                    message: "3桁を超えるアラビア数字が使われています",
+                    line: 1,
+                    column: 2
+                },
+                {
+                    message: "3桁を超えるアラビア数字が使われています",
+                    line: 1,
+                    column: 11
+                }
+            ]
         }
     ]
 });
